38eed161e50510a80686e5286213ff4f8e5442ca
Hansen <hansenca@hawaii.edu>
2022-01-29 21:32:47 -1000

    Issue #12: Removed conflict with mergers

--- app/imports/api/matrp/MATRP.js
+++ app/imports/api/matrp/MATRP.js
@@ -2,7 +2,6 @@ import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../stuff/StuffCollection';
 import { AdminProfiles } from '../user/AdminProfileCollection';
 import { UserProfiles } from '../user/UserProfileCollection';
-import { VolunteerProfiles } from '../user/VolunteerCollection';
 
 class MATRPClass {
   collections;
@@ -17,7 +16,6 @@ class MATRPClass {
       AdminProfiles,
       Stuffs,
       UserProfiles,
-      VolunteerProfiles,
     ];
     /*
      * A list of collection class instances in the order required for them to be sequentially loaded from a file.
