dca7bb808304b87251d94dc3c332eb212ffe3b8d
chadoshiro <oshiroc3@hawaii.edu>
2022-02-15 12:35:24 -1000

    Deleted Segment from imports

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Header, Image, Button, Segment } from 'semantic-ui-react';
+import { Grid, Header, Image, Button } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 import { PAGE_IDS } from '../utilities/PageIDs';
 
