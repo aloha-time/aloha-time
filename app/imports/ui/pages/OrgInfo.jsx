2333f69d9ffb70335866997510c81992bbd939bc
jon-valencia <jon.valencia@outlook.com>
2022-02-15 00:00:46 -1000

    OrgInfo.jsx now only shows the correct org

--- app/imports/ui/pages/OrgInfo.jsx
+++ app/imports/ui/pages/OrgInfo.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Container, Loader, Grid, Embed, Header, Feed } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router';
 import { Stuffs } from '../../api/stuff/StuffCollection';
 import { OrganizationProfiles } from '../../api/user/OrganizationProfileCollection'
 import OrgItem2 from '../components/OrgItem2';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-const OrgInfo = ({ ready, orgs }) => ((ready) ? (
+const OrgInfo = ({ ready, doc }) => ((ready) ? (
   <Container>
     <Grid columns='equal'>
       <Grid.Column>
-        {orgs.map((org) => <OrgItem2 key={org._id} org={org}/>)}
+        <OrgItem2 org={doc}/>
       </Grid.Column>
       <Grid.Column>
         <Feed>
@@ -48,18 +49,25 @@ const OrgInfo = ({ ready, orgs }) => ((ready) ? (
 OrgInfo.propTypes = {
   orgs: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
+  doc: PropTypes.object,
 };
 
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const { _id } = useParams();
+  const documentId = _id;
   // Get access to Stuff documents.
   const subscription = OrganizationProfiles.subscribeOrg();
   // Determine if the subscription is ready
   const ready = subscription.ready();
   // Get the Stuff documents and sort them by name.
   const orgs = OrganizationProfiles.find({}, { sort: { orgName: 1 } }).fetch();
+  // Get the document 
+  const doc = orgs.find((org) => org._id === documentId);
   return {
     orgs,
     ready,
+    doc,
   };
 })(OrgInfo);
