dadde1c9b69a4080f8a6e7874280ebe98b326398
rainllo <rjavillo@hawaii.edu>
2022-03-02 22:36:31 -1000

    Issue #65: Added filter by title.

--- app/imports/ui/pages/ListOpportunity.jsx
+++ app/imports/ui/pages/ListOpportunity.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Container, Header, Loader, Card, Grid, Button } from 'semantic-ui-react';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { AutoForm, SubmitField } from 'uniforms-semantic';
+import { _ } from 'meteor/underscore';
+import { Container, Header, Loader, Card, Grid, Button, Segment } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { Opportunities } from '../../api/opportunity/OpportunitiesCollection';
 import OpportunityItem from '../components/OpportunityItem';
 import { PAGE_IDS } from '../utilities/PageIDs';
+import MultiSelectField from '../components/form-fields/MultiSelectField';
+
+/** Create a schema to specify the structure of the data to appear in the form. */
+const makeSchema = (allOpportunities) => new SimpleSchema({
+  opportunities: { type: Array, label: 'Opportunities', optional: true },
+  'opportunities.$': { type: String, allowedValues: allOpportunities },
+});
 
 /** Renders a table containing all the Opportunity documents. Use <OpportunityItem> to render each row. */
 class ListOpportunity extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { opportunities: [] };
+  }
+
+  submit(data) {
+    this.setState({ opportunities: data.opportunities || [] });
+  }
+
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
@@ -16,14 +37,18 @@ class ListOpportunity extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    const allOpportunities = _.pluck(Opportunities.find({}, {}).fetch(), 'title');
+    const formSchema = makeSchema(allOpportunities);
+    const bridge = new SimpleSchema2Bridge(formSchema);
+    const opportunityList = Opportunities.find({ title: { $in: this.state.opportunities } }, {}).fetch();
     return (
       <Container id={PAGE_IDS.LIST_OPPORTUNITY}>
         <div className="organization-sign-up-top">
           <Header as="h2" textAlign="center" inverted>
-            Browse Opportunities
+              Browse Opportunities
           </Header>
           <Header as="h5" textAlign="center" inverted>
-            Volunteer for upcoming or existing events
+              Volunteer for upcoming or existing events
           </Header>
         </div>
         <br/>
@@ -32,7 +57,13 @@ class ListOpportunity extends React.Component {
             src="https://www.openstreetmap.org/export/embed.html?bbox=-158.42234810348603%2C21.16792215058402%2C-157.6793976640329%2C21.7615698948541&amp;layer=mapnik"/>
         </div>
         <br/>
-        <Card.Group stackable itemsPerRow={3} centered>{this.props.opportunities.map((opportunities, index) => <OpportunityItem
+        <AutoForm style={{ paddingBottom: '20px' }} schema={bridge} onSubmit={data => this.submit(data)} >
+          <Segment>
+            <MultiSelectField id='opportunities' name='opportunities' showInlineError={true} placeholder={'Pick an Opportunity'}/>
+            <SubmitField id='submit' value='Submit'/>
+          </Segment>
+        </AutoForm>
+        <Card.Group stackable itemsPerRow={3} centered>{opportunityList.map((opportunities, index) => <OpportunityItem
           key={index}
           opportunity={opportunities}/>)}
         </Card.Group>
@@ -43,10 +74,10 @@ class ListOpportunity extends React.Component {
             <Grid.Column>
               <Button animated='vertical' fluid color='blue' as={NavLink} exact to="/add-opportunity">
                 <Button.Content hidden>
-                  Add a new opportunity
+                    Add a new opportunity
                 </Button.Content>
                 <Button.Content visible>
-                  Are you an organization?
+                    Are you an organization?
                 </Button.Content>
               </Button>
             </Grid.Column>
