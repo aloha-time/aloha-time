913355b0b16d60ed8335f9255a5424c5a4f35cb5
Hansen <hansenca@hawaii.edu>
2022-03-22 20:34:50 -1000

    Issue #64: Search feature working along with organization collection utilization

--- app/imports/ui/components/AdminTabs.jsx
+++ app/imports/ui/components/AdminTabs.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Tab, Item, Segment, Header, Grid } from 'semantic-ui-react';
+import { Tab, Item } from 'semantic-ui-react';
 import OrgVerifyList from './OrgVerifyList';
 import VolunteerVerifyList from './VolunteerVerifyList';
 import AdminSettingsForm from './AdminSettingsForm';
-import AdminOrgSearch from './AdminOrgSearch';
 import AdminVolunteerSearch from './AdminVolunteerSearch';
 
-const panes = ({ organizations }) => [
+const panes = [
   // eslint-disable-next-line react/display-name
   { menuItem: 'Manage Account', render: () => <Tab.Pane>
     <AdminSettingsForm/>
   </Tab.Pane> },
   // eslint-disable-next-line react/display-name
   { menuItem: 'Manage Organizations', render: () => <Tab.Pane>
-    <AdminOrgSearch source={organizations}/>
-    <Item.Group divided>
-      <OrgVerifyList/>
-      <OrgVerifyList/>
-      <OrgVerifyList/>
-      <OrgVerifyList/>
-      <OrgVerifyList/>
-    </Item.Group>
+    <OrgVerifyList/>
   </Tab.Pane> },
   // eslint-disable-next-line react/display-name
   { menuItem: 'Manage Volunteers', render: () => <Tab.Pane>
@@ -36,12 +27,6 @@ const panes = ({ organizations }) => [
   </Tab.Pane> },
 ];
 
-panes.displayName = 'panes';
-
-const AdminTabs = ({ organizations }) => <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes(organizations)}/>;
-
-AdminTabs.propTypes = {
-  organizations: PropTypes.array.isRequired,
-};
+const AdminTabs = () => <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes}/>;
 
 export default AdminTabs;
--- app/imports/ui/pages/AdminPage.jsx
+++ app/imports/ui/pages/AdminPage.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
-import { withTracker } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
-import { Container, Loader } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import AdminTabs from '../components/AdminTabs';
-import { OrganizationProfiles } from '../../api/user/OrganizationProfileCollection';
 
-const AdminPage = ({ ready, organization }) => ((ready) ? (
+const AdminPage = () => (
   <Container>
     <div>
       <h1 className="ui center aligned header">
         Admin Page
       </h1>
     </div>
-    <AdminTabs organizations={organization}/>
+    <AdminTabs/>
   </Container>
-) : <Loader active>Getting data</Loader>);
+);
 
-AdminPage.propTypes = {
-  organization: PropTypes.array.isRequired,
-  ready: PropTypes.bool.isRequired,
-};
-
-export default withTracker(() => {
-  const subscription = OrganizationProfiles.subscribe();
-  const ready = subscription.ready();
-  const organization = OrganizationProfiles.find({}, { sort: { organizationName: 1 } }).fetch();
-  return {
-    ready,
-    organization,
-  };
-})(AdminPage);
+export default AdminPage;
