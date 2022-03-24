def3167a6f77c0389460678d035f5cbe4311a262
Marissa <mhalim@hawaii.edu>
2022-03-23 19:12:25 -1000

    Finished adding opportunity log

--- app/imports/ui/pages/ShowVolunteerProfile.jsx
+++ app/imports/ui/pages/ShowVolunteerProfile.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Container, Loader, Button, Header, Image, Grid, List, Segment } from 'semantic-ui-react';
+import { Container, Loader, Button, Header, Image, Grid, List, Segment, Divider, Item } from 'semantic-ui-react';
+import { CarouselProvider, Slide, Slider } from 'pure-react-carousel';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { Link } from 'react-router-dom';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { VolunteerProfiles } from '../../api/user/VolunteerProfileCollection';
+import CustomDotGroup from '../components/CustomDotGroup';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 const headerStyle = { padding: '1em' };
 const columnStyle = { paddingTop: '9em' };
 const margSides = { marginLeft: '4em', marginRight: '4em' };
+const margCarousel = { paddingTop: '4em', marginLeft: '3.25em', marginRight: '3.25em' };
 
 /** Renders the page for volunteer profile document. */
 const ShowVolunteerProfile = ({ volProfile, ready }) => ((ready) ? (
@@ -29,14 +33,12 @@ const ShowVolunteerProfile = ({ volProfile, ready }) => ((ready) ? (
         <p>Date of Birth: {volProfile.dateOfBirth}</p>
       </Grid.Column>
       <Grid.Column verticalAlign='middle' width={4} style={columnStyle}>
-        <Segment>
-          <Header as='h1' textAlign='center'>
+        <Header as='h1' textAlign='center'>
             Hours volunteered
-          </Header>
-          <Header as='h2' textAlign='center'>
+        </Header>
+        <Header as='h2' textAlign='center'>
             20 hours
-          </Header>
-        </Segment>
+        </Header>
       </Grid.Column>
     </Grid>
     <Segment.Group horizontal style={margSides}>
@@ -78,6 +80,60 @@ const ShowVolunteerProfile = ({ volProfile, ready }) => ((ready) ? (
         </List>
       </Segment>
     </Segment.Group>
+    <Grid style={margCarousel} >
+      <Grid.Column width={8} className='volunteerCarousel'>
+        <Header as='h1' textAlign='center'>
+          Memories
+        </Header>
+        <CarouselProvider
+          totalSlides={6}
+          isIntrinsicHeight={true}
+        >
+          <Slider>
+            <Slide index={0}>
+              <Image src="https://149366112.v2.pressablecdn.com/wp-content/uploads/2014/05/1959781_10152032231622849_2043556589_n.jpg" />
+            </Slide>
+            <Slide index={1}>
+              <Image src="https://www.peggyadams.org/sites/default/files/images/2018-03/volunteer-PA.jpg " />
+            </Slide>
+            <Slide index={2}>
+              <Image src="https://humaneloudoun.org/wp-content/uploads/2020/01/AnimalServicesVolunteer.jpg" />
+            </Slide>
+            <Slide index={3}>
+              <Image src="https://images.everydayhealth.com/images/coronavirus/how-and-where-to-volunteer-safely-during-the-pandemic-rm-722x406.jpg?sfvrsn=e1c370c2_1" />
+            </Slide>
+            <Slide index={4}>
+              <Image src="https://www.californiavolunteers.ca.gov/wp-content/uploads/2021/02/Matthew.png" />
+            </Slide>
+            <Slide index={5}>
+              <Image src="https://coda.newjobs.com/api/imagesproxy/ms/niche/images/articles/Liz/volunteering.jpg" />
+            </Slide>
+          </Slider>
+          <Divider />
+          <CustomDotGroup slides={6} />
+        </CarouselProvider>
+        <Button> Upload Image </Button>
+      </Grid.Column>
+      <Grid.Column width={6} floated='right'>
+        <Header as='h1' textAlign='center'>
+          Opportunity Log
+        </Header>
+        <Item.Group relaxed>
+          <Item>
+            <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+            <Item.Content verticalAlign='middle'>
+              <Item.Header as='a'>Opportunity 1</Item.Header>
+            </Item.Content>
+          </Item>
+          <Item>
+            <Item.Image size='tiny' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+            <Item.Content verticalAlign='middle'>
+              <Item.Header as='a'>Opportunity 2</Item.Header>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Grid.Column>
+    </Grid>
     <div className="my-organization-bottom" >
       <Button inverted size= "big" color="blue"><Link to="/edit-my-profile">Edit My Profile</Link></Button>
     </div>
