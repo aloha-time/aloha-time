2b7e9289e8f1758773e95b29af42ed0c78831757
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2022-03-30 10:22:20 -1000

    Update MyUrl.jsx

--- app/imports/ui/components/MyUrl.jsx
+++ app/imports/ui/components/MyUrl.jsx
@@ -1,6 +1,6 @@
 //  we need to base64 it to Data URI
 export const MyUrl = (image) => {
-  if (image.includes('/images')) {
+  if (image.includes('/images') || image.includes('http')) {
     return image;
   }
   return `data:"image/jpeg"; charset=utf-8;base64,${image.toString('base64')}`;
