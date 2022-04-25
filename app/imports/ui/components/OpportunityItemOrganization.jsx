7139263dbbb2e7f15fe751fe2ba56713726ae9f3
rainllo <rjavillo@hawaii.edu>
2022-04-24 22:33:23 -1000

    Issue #108: Reworked OpportunityItemOrganization.jsx into functional component

--- app/imports/ui/components/OpportunityItemOrganization.jsx
+++ app/imports/ui/components/OpportunityItemOrganization.jsx
@@ -1,82 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Accordion, Button, Card, Icon, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, NavLink } from 'react-router-dom';
 import { MyUrl } from './MyUrl';
 
-/** Renders a single row in the List Opportunity table. See pages/ListOpportunity.jsx. */
-class OpportunityItemOrganization extends React.Component {
-  state = { activeIndex: -1 }
+/** Renders a single row in the List Opportunity Organization table. See pages/ListOpportunityOrganization.jsx. */
+const OpportunityItemOrganization = ({ opportunity }) => {
+  const [activeIndex, setActiveIndex] = useState(-1);
 
-  handleClick = (e, titleProps) => {
-    const { index } = titleProps;
-    const { activeIndex } = this.state;
-    const newIndex = activeIndex === index ? -1 : index;
+  const handleClick = (e, titleProps) => {
+    e.preventDefault();
+    // console.log(titleProps);
+    const newIndex = activeIndex === titleProps.index ? -1 : titleProps.index;
+    setActiveIndex(newIndex);
+  };
 
-    this.setState({ activeIndex: newIndex });
-  }
-
-  render() {
-    const OpportunityInfo = this.props.opportunity;
-    const { activeIndex } = this.state;
-    return (
-      <Card link fluid>
-        <Image large src={MyUrl(OpportunityInfo.coverImage)} wrapped ui={false} as={NavLink}
-          exact to={`/view-opportunity/${this.props.opportunity._id}`}/>
-        <Card.Content>
-          <Card.Header>
-            <h1>{OpportunityInfo.title}</h1>
-          </Card.Header>
-          <Card.Meta>
-            <Icon name='calendar alternate'/>
-            {OpportunityInfo.startDate.toLocaleDateString('en-US')} - {OpportunityInfo.endDate.toLocaleDateString('en-US')}
-          </Card.Meta>
-          <Card.Meta>
-            <Icon name='clock'/>
-            {OpportunityInfo.startDate.toLocaleTimeString('en-US').replace(/:\d+ /, ' ')} - {OpportunityInfo.endDate.toLocaleTimeString('en-US').replace(/:\d+ /, ' ')}</Card.Meta>
-          <Card.Meta>
-            <Icon name='map marker alternate'/>
-            {OpportunityInfo.location}
-          </Card.Meta>
-          <Card.Meta>Category: {OpportunityInfo.category}</Card.Meta>
-        </Card.Content>
-        <Card.Content>
-          <Accordion>
-            <Accordion.Title
-              active={activeIndex === 0}
-              index={0}
-              onClick={this.handleClick}
-            >
-              <h4><Icon name='dropdown'/>
-                See more</h4>
-            </Accordion.Title>
-            <Accordion.Content active={activeIndex === 0}>
-              <p>
-                {OpportunityInfo.description}
-              </p>
-            </Accordion.Content>
-          </Accordion>
-        </Card.Content>
-        <Card.Content extra>
-          <Button color='blue' as={NavLink} exact to={`/edit-opportunity/${this.props.opportunity._id}`}>
+  return (
+    <Card link fluid>
+      <Image large src={MyUrl(opportunity.coverImage)} wrapped ui={false} as={NavLink}
+        exact to={`/view-opportunity/${opportunity._id}`}/>
+      <Card.Content>
+        <Card.Header>
+          <h1>{opportunity.title}</h1>
+        </Card.Header>
+        <Card.Meta>
+          <Icon name='calendar alternate'/>
+          {opportunity.startDate.toLocaleDateString('en-US')} - {opportunity.endDate.toLocaleDateString('en-US')}
+        </Card.Meta>
+        <Card.Meta>
+          <Icon name='clock'/>
+          {opportunity.startDate.toLocaleTimeString('en-US').replace(/:\d+ /, ' ')} - {opportunity.endDate.toLocaleTimeString('en-US').replace(/:\d+ /, ' ')}
+        </Card.Meta>
+        <Card.Meta>
+          <Icon name='map marker alternate'/>
+          {opportunity.location}
+        </Card.Meta>
+        <Card.Meta>Category: {opportunity.category}</Card.Meta>
+      </Card.Content>
+      <Card.Content>
+        <Accordion>
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={handleClick}
+          >
+            <h4>
+              <Icon name='dropdown'/>
+                See more
+            </h4>
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            <p>
+              {opportunity.description}
+            </p>
+          </Accordion.Content>
+        </Accordion>
+      </Card.Content>
+      <Card.Content extra>
+        <Button color='blue' as={NavLink} exact to={`/edit-opportunity/${opportunity._id}`}>
             Edit
-          </Button>
-          <Button color='red'>
+        </Button>
+        <Button color='red'>
             Delete
-          </Button>
-        </Card.Content>
-        <Card.Content extra>
-          <Icon name='user'/>
-          {OpportunityInfo.owner}
-        </Card.Content>
-      </Card>
-    );
-  }
-}
+        </Button>
+      </Card.Content>
+    </Card>
+  );
+};
 
 // Require a document to be passed to this component.
 OpportunityItemOrganization.propTypes = {
-  opportunity: PropTypes.object.isRequired,
+  opportunity: PropTypes.shape({
+    title: PropTypes.string,
+    opportunityType: PropTypes.string,
+    startDate: PropTypes.date,
+    endDate: PropTypes.date,
+    recurring: PropTypes.string,
+    description: PropTypes.string,
+    category: PropTypes.array,
+    location: PropTypes.string,
+    contactName: PropTypes.string,
+    contactPosition: PropTypes.string,
+    email: PropTypes.string,
+    phone: PropTypes.string,
+    website: PropTypes.string,
+    coverImage: PropTypes.string,
+    galleryImg1: PropTypes.string,
+    galleryImg2: PropTypes.string,
+    galleryImg3: PropTypes.string,
+    galleryImg4: PropTypes.string,
+    ageGroup: PropTypes.array,
+    environment: PropTypes.array,
+    owner: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
 };
 
 // Wrap this component in withRouter since we use the <Link> React Router element.
