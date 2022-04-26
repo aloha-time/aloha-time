971371153d51d6162b3a60c5645df5b147b7c5e5
rainllo <rjavillo@hawaii.edu>
2022-04-25 19:27:17 -1000

    Issue #135: CalendarSchedule.jsx now takes in even dates from the collection. Removed CalendarItem.jsx component

--- app/imports/ui/pages/CalendarSchedule.jsx
+++ app/imports/ui/pages/CalendarSchedule.jsx
@@ -1,75 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { _ } from 'meteor/underscore';
 import { Button, Container, Header } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
-import DatePicker from 'react-datepicker';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Meteor } from 'meteor/meteor';
-import { Roles } from 'meteor/alanning:roles';
 import 'react-datepicker/dist/react-datepicker.css';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import { Opportunities } from '../../api/opportunity/OpportunitiesCollection';
-import { ROLE } from '../../api/role/Role';
 
 const localizer = momentLocalizer(moment);
-
-const events = [
-  {
-    title: 'Health Help',
-    start: new Date(2022, 3, 15),
-    end: new Date(2022, 3, 28),
-  },
-  {
-    title: 'Beach Cleanup',
-    start: new Date(2022, 3, 17),
-    end: new Date(2022, 3, 25),
-  },
-  {
-    title: 'Food Drive',
-    start: new Date(2022, 3, 18),
-    end: new Date(2022, 3, 28),
-  },
-  {
-    title: 'Virtual Dog Walk',
-    start: new Date(2022, 3, 5),
-    end: new Date(2022, 3, 10),
-  },
-];
 
 /** The calendar scheduler. */
 const CalendarSchedule = () => {
 
-  const allTitles = _.pick(Opportunities.find({}, {}).fetch(), 'title');
-  const allStartDates = _.pick(Opportunities.find({}, {}).fetch(), 'startDate');
-  const allEndDates = _.pick(Opportunities.find({}, {}).fetch(), 'endDate');
-  const allNeeded = Opportunities.find({}, { fields: { title: 1, startDate: 1, endDate: 1 } }).fetch();
+  /* find the title, startDate and endDate from each event in the OpportunitiesCollection */
+  const eventFields = Opportunities.find({}, { fields: { title: 1, startDate: 1, endDate: 1 } }).fetch();
 
-  /* const newArray = allNeeded.map(({
-    endDate: end,
-    ...rest
-  }) => ({
-    end,
-    ...rest,
-  })); */
+  /* use map to rename the fields title to title, startDate to start, endDate to end and remove _id */
+  const myEventsList = eventFields.map(({ title, startDate, endDate }) => ({ title: title, start: startDate, end: endDate }));
 
-  const newArray = allNeeded.map(({ title, startDate, endDate }) => ({ title: title, start: startDate, end: endDate }));
-
-  const [newEvent, setNewEvent] = useState({ title: '', start: '', end: '' });
-  const [allEvents, setAllEvents] = useState(events);
-
-  function handleAddEvent() {
-    setAllEvents([...allEvents, newEvent]);
-  }
-
-  console.log(allTitles);
-  console.log(allStartDates);
-  console.log(allEndDates);
-  console.log(allNeeded);
-  console.log(newArray);
   return (
     <Container id={PAGE_IDS.CALENDAR_SCHEDULE}>
       <div className="organization-sign-up-top">
@@ -89,20 +40,9 @@ const CalendarSchedule = () => {
       <br/>
       <br/>
       <div>
-        {Roles.userIsInRole(Meteor.userId(), [ROLE.ORGANIZATION]) ? (
-          <div>
-            <input type="text" placeholder="Add Title"
-              value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}/>
-            <DatePicker showTimeSelect placeholderText="Start Date"
-              selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
-            <DatePicker showTimeSelect placeholderText="End Date"
-              selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
-            <Button style={{ marginTop: '10px', marginBottom: '10px' }} onClick={handleAddEvent}>Add Event</Button>
-          </div>)
-          : ''}
         <Calendar
           localizer={localizer}
-          events={newArray}
+          events={myEventsList}
           startAccessor="start"
           endAccessor="end"
           style={{ height: 600 }}
