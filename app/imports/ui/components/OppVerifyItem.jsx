9322c95343628828b201dc40d9af592e441a9808
Hansen <hansenca@hawaii.edu>
2022-04-26 17:09:12 -1000

    Added more information to each opportunity

--- app/imports/ui/components/OppVerifyItem.jsx
+++ app/imports/ui/components/OppVerifyItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Item, Segment } from 'semantic-ui-react';
+import { Button, Icon, Item, Segment } from 'semantic-ui-react';
 import swal from 'sweetalert';
 import PropTypes from 'prop-types';
 import { MyUrl } from './MyUrl';
@@ -36,8 +36,27 @@ const OppVerifyItem = ({ opportunity }) => {
       <Item.Image src={MyUrl(opportunity.coverImage)}/>
       <Item.Content verticalAlign='middle'>
         <Item.Header as='a'>{opportunity.title}</Item.Header>
+        <Item.Description>
+          <span>{opportunity.description}</span>
+        </Item.Description>
         <Item.Meta>
           <span>{opportunity.opportunityType}</span>
+        </Item.Meta>
+        <Item.Meta>
+          <Icon name='calendar alternate'/>
+          {opportunity.startDate.toLocaleDateString('en-US')} - {opportunity.endDate.toLocaleDateString('en-US')}
+        </Item.Meta>
+        <Item.Meta>
+          <Icon name='clock'/>
+          {opportunity.startDate.toLocaleTimeString('en-US').replace(/:\d+ /, ' ')} - {opportunity.endDate.toLocaleTimeString('en-US').replace(/:\d+ /, ' ')}
+        </Item.Meta>
+        <Item.Meta>
+          <Icon name='map marker alternate'/>
+          {opportunity.location}
+        </Item.Meta>
+        <Item.Meta>Category: {opportunity.category}</Item.Meta>
+        <Item.Meta>
+          <Icon floated='right' name='globe'>{opportunity.website}</Icon>
         </Item.Meta>
         <Item.Extra>
           <Segment.Inline>
@@ -58,6 +77,12 @@ OppVerifyItem.propTypes = {
     title: PropTypes.string,
     opportunityType: PropTypes.string,
     coverImage: PropTypes.string,
+    startDate: PropTypes.date,
+    endDate: PropTypes.date,
+    location: PropTypes.string,
+    category: PropTypes.array,
+    description: PropTypes.string,
+    website: PropTypes.string,
   }).isRequired,
 };
 
